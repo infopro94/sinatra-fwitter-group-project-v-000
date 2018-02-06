3d0e8494000b98f9169817de912e96d1404f6504
github username <github email address>
2018-02-05 20:48:13 -0600

    index contains old code from layout

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,49 +8,55 @@ class UsersController < ApplicationController
   	 end
 
   	get '/signup' do
-  		if logged_in?
-          redirect to '/tweets'
-        else
-          erb :'/users/create_user' #, locals: {message: "Please sign up to sign in"}
-  		# else redirect to "/users/#{user.slug}"
+  		if !logged_in?
+  			erb :'users/create', locals: {message: "Please sign up to sign in"}
+        # binding.pry
+  		else
+  			redirect to "/tweets"
+        
   		end
 	   end
 
 
     post '/signup' do
-      if logged_in?
-        redirect '/tweets'
-    	elsif params[:username] == "" || params[:email] == "" || params[:password] == "" 
-    		redirect to '/signup'
+    	if params[:username] == "" || params[:email] == "" || params[:password] == "" 
+    		redirect to "/signup"
+          
     	else
-         @user = User.create(params) 
-           @user.save 
-           session[:id]  = @user.id
-          redirect to '/tweets'
+      		# @user = User.new(:username => params[:username], :email => params[:email], :password => params[:password])
+
+      		# @user.save
+      		# session[:user_id] = @user.id
+          redirect to "/tweets"
     	end
   	end
 
   	get '/login' do
   		if !logged_in?
-  			erb :'/users/login'
+  			erb :'users/login'
   		else
   			redirect to '/tweets'
   		end
   	end
 
-  	post '/login' do
-  		 @user = User.find_by(:username => params[:username])
-  		if @user && @user.authenticate(params[:password])
-  			session[:user_id] = user.id
+  	post '/login' do #should not let a logged in user view the signup page
+  		   @user = User.find_by(params[:username])
+  		if logged_in?
+     #    user && user.authenticate(params[:password])
+  			# session[:user_id] = user.id
   			redirect to '/tweets'
-      else
-        redirect to '/login'
+  		else
+  			redirect to '/signup'
   		end
   	end
 
     get '/logout' do
-    		session.clear
-        redirect to '/login'
+    	if logged_in?
+    		session.destroy
+    		redirect to '/login'
+    	else
+    		redirect to '/'
+        end
     end
 
 end
